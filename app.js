9975ed867ddaf81a54c628a8bf8732a88a638040
kharman82 <kharman82@gmail.com>
2020-03-12 15:57:29 -0700

    first half of the contructor built lab07

--- app.js
+++ app.js
@@ -2,57 +2,66 @@
 
 // define constructor
 
-var cookies =[];
+var stores =[];
 
 // new code added and replaced var's
-function seattle(minCust, maxCust, avgCookies, time, hoursOpen, cookieArray,cookieSum,) {
-    location: 'seattle',
-    this.minCust = minCust; 23,
-    this.maxCust = maxCust; 65,
-    this.avgCookies = avgCookies; 6.3,
-    this.time = time; ['6am ', '7am ', '8am ', '9am ', '10am ', '11am ', '12pm ', '1pm ', '2pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ',],
-    this.hoursOpen = hoursOpen; 14,
-    this.cookieArray = cookieArray; [],
-    this.cookieSum = cookieSum; 0,
-    cookies.push(this);
+function StoreCreator (location, minCust, maxCust, avgCookies, hoursOpen) {
+    this.location = location;
+    this.minCust = minCust; 
+    this.maxCust = maxCust; 
+    this.avgCookies = avgCookies;
+    var time = ['6am ', '7am ', '8am ', '9am ', '10am ', '11am ', '12pm ', '1pm ', '2pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ',];
+    this.time = time; 
+    this.hoursOpen = hoursOpen; 
+    stores.push(this);
     }
 
-   seattle.prototype.random = function() {
-        for (var i =0; i < this.hoursOpen; i++) {
-    var total= Math.floor(Math.random() * ( this.maxCust - this.minCust) + this.minCust + 1);
-    var cphr= Math.floor(total * this.avgCookies)
-    var totalCookie1= [total, cphr]
-    console.log (totalCookie1);
-    this.cookieArray.push(cphr);
 
-        };
-    };
+    
+    var seattle = new StoreCreator('seattle', 23, 65, 6.3, 14);
+    var tokyo = new StoreCreator('tokyo', 3, 24, 1.2, 14);
+    var dubai = new StoreCreator('dubai', 11, 38, 3.7, 14);
+    var paris = new StoreCreator('paris', 20, 38, 2.3, 14);
+    var lima = new StoreCreator('lima', 2, 16, 4.6, 14);
+    console.log (stores);
+
+    
+// seattle.prototype.random = function() {
+//         for (var i =0; i < this.hoursOpen; i++) {
+//     var total= Math.floor(Math.random() * ( this.maxCust - this.minCust) + this.minCust + 1);
+//     var cphr= Math.floor(total * this.avgCookies)
+//     var totalCookie1= [total, cphr]
+//     console.log (totalCookie1);
+//     this.cookieArray.push(cphr);
+
+//         };
+//     };
     
 
-    seattle.prototype.daily = function() {
-        var hourlyCookies= this.random();
-            for (var i =0; i < this.hoursOpen; i++) {
-                var seattleList= document.getElementById('seattleList');
-                var seattleChildEl= document.createElement('li');
-                    seattleList.appendChild(seattleChildEl);
-                    seattleChildEl.textContent = (this.time[i] + this.cookieArray[i] + ' cookies ');
+//     seattle.prototype.daily = function() {
+//         var hourlyCookies= this.random();
+//             for (var i =0; i < this.hoursOpen; i++) {
+//                 var seattleList= document.getElementById('seattleList');
+//                 var seattleChildEl= document.createElement('li');
+//                     seattleList.appendChild(seattleChildEl);
+//                     seattleChildEl.textContent = (this.time[i] + this.cookieArray[i] + ' cookies ');
      
-    };
-   }; 
-    seattle.prototype.dailySum = function() {
-       this.random();
-       var something =0;
-    for (var i =0; i < this.cookieArray.length; i++); {
-        something = (this.cookieArray[i] + something);
-            console.log(something);
+//     };
+//    }; 
+//     seattle.prototype.dailySum = function() {
+//        this.random();
+//        var something =0;
+//     for (var i =0; i < this.cookieArray.length; i++); {
+//         something = (this.cookieArray[i] + something);
+//             console.log(something);
     
-    };
-   };
+//     };
+//    };
 
-var daily = new cookieArray('');
-    console.log (seattle.cookieArray);
-var dailySum = new dailySum('');
-    console.log(seattle.dailySum());
+// var daily = new cookieArray('');
+//     console.log (seattle.cookieArray);
+// var dailySum = new dailySum('');
+//     console.log(seattle.dailySum());
 
 
 // var tokyo = {
@@ -60,10 +69,9 @@ var dailySum = new dailySum('');
 //     minCust: 3,
 //     maxCust: 24,
 //     avgCookies: 1.2,  
-//     time: ['6am ', '7am ', '8am ', '9am ', '10am ', '11am ', '12pm ', '1pm ', '2pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ',],
+    // var time: ['6am ', '7am ', '8am ', '9am ', '10am ', '11am ', '12pm ', '1pm ', '2pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ',];
 //     hoursOpen: 14,
-//     cookieArray: [],
-//     cookieSum: 0, 
+
 
 // random: function() {
 //     for (var i =0; i < this.hoursOpen; i++) {
